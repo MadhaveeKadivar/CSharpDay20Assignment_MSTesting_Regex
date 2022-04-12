d92ea115e6b037223ea2831d6927b540e1475f83
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-12 11:04:50 +0530

    [Madhavee] Add MSTesting in User's Registration Validation using regular expression

---
+++ MsTestingRegex/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MsTestingRegex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MsTestingRegex")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6b7d83a-3f9c-4279-8341-ec73de4b2091")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MsTestingRegex/UserValidation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTesting_RegularExpression;
+using System;
+
+namespace MsTestingRegex
+{
+    [TestClass]
+    public class UserValidation
+    {
+        [TestMethod]
+        //Checking for multiple first name
+        [DataRow("Madhavee",true)]
+        [DataRow("Ab", false)]
+        [DataRow("madhavee",false)]
+        public void GivenFirstNameValidation(string firstName,bool expected) // Testing for Firstname Validation
+        {
+            //Arrange
+            Validation validation = new Validation();
+            //Act
+            bool actual = validation.FirstNameValidation(firstName);
+            //Assert
+            Assert.AreEqual(expected, actual);            
+        }        
+    }
+}
